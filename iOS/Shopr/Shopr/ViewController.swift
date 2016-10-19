b6d1590ac37b206c326fbf2b76e2f28831d3441e
Sahil Pujari <pujari@purdue.edu>
2016-10-19 00:27:51 -0400

    1. Splash Screen

--- iOS/Shopr/Shopr/ViewController.swift
+++ iOS/Shopr/Shopr/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logo : UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        UIView.animate(withDuration: 1.0, delay: 1.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
+                self.logo.alpha = 1.0
+            }, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
