78a1afb49656a35c67d6668006fe5e50b72dd319
Neil Allison <neiljallison@gmail.com>
2016-11-19 23:54:39 -0500

    Removing unused imports.

--- android/ShoprApp/app/src/main/java/shopr/com/shoprapp/fragment/WishListFragment.java
+++ android/ShoprApp/app/src/main/java/shopr/com/shoprapp/fragment/WishListFragment.java
@@ -13,8 +13,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
